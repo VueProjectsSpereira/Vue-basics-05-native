b7f2f0c4b15fdb96679d24bd059156566afed7c6
Saul Pereira <saul_pereira@hotmail.com>
2024-09-12 17:35:11 -0600

    33C. Working with Watchers
    33C. Working with Watchers

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ const app = Vue.createApp({
   watch: {
     counter(value) {
       if (value > 50) {
-        this.counter = 0;
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        }, 2000);
       }
     },
     // name(value) {
