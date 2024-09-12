6c8249eb8cc4e15db4c0793c0c6cdfce40081369
Saul Pereira <saul_pereira@hotmail.com>
2024-09-12 17:11:31 -0600

    32. Introducing Computed Properties
    32. Introducing Computed Properties

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const app = Vue.createApp({
       name: "",
     };
   },
+  computed: {
+    fullname() {
+      console.log("Running again...");
+      if (this.name === "") {
+        return "";
+      }
+      return this.name + " " + "Pereira";
+    },
+  },
   methods: {
     outputFullname() {
       if (this.name === "") {
